9bdce6c67f981c2aef0b1f03f7ea1ab3ca078263
Vishal Ghosh <avatarvishal1551@gmail.com>
2024-03-23 19:34:28 +0530

    Initial Files

--- lib/common/entities/course.dart
+++ lib/common/entities/course.dart
@@ -1,5 +1,4 @@
-
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CourseRequestEntity {
   int? id;
@@ -9,8 +8,8 @@ class CourseRequestEntity {
   });
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-  };
+        "id": id,
+      };
 }
 
 class SearchRequestEntity {
@@ -21,8 +20,8 @@ class SearchRequestEntity {
   });
 
   Map<String, dynamic> toJson() => {
-    "search": search,
-  };
+        "search": search,
+      };
 }
 
 class CourseListResponseEntity {
@@ -36,11 +35,23 @@ class CourseListResponseEntity {
     this.data,
   });
 
+  // json['data'] has maps internally , so we need to go through fromJson Method
   factory CourseListResponseEntity.fromJson(Map<String, dynamic> json) =>
       CourseListResponseEntity(
         code: json["code"],
         msg: json["msg"],
-        data: json["data"] == null ? [] : List<CourseItem>.from(json["data"].map((x) => CourseItem.fromJson(x))),
+        data: json["data"] == null
+            ? []
+            : List<CourseItem>.from(
+                json["data"].map(
+                  // data is in the form of [
+                  // {...} , {...}
+                  // ]
+                  // X defines this each {} , which then stores in CourseItem class
+                  // which has all the specific items referencing to the specific data values
+                  (x) => CourseItem.fromJson(x),
+                ),
+              ),
       );
 }
 
@@ -64,8 +75,6 @@ class CourseDetailResponseEntity {
       );
 }
 
-
-
 class AuthorRequestEntity {
   String? token;
 
@@ -74,9 +83,10 @@ class AuthorRequestEntity {
   });
 
   Map<String, dynamic> toJson() => {
-    "token": token,
-  };
+        "token": token,
+      };
 }
+
 //api post response msg
 class AuthorResponseEntity {
   int? code;
@@ -96,8 +106,6 @@ class AuthorResponseEntity {
         data: AuthorItem.fromJson(json["data"]),
       );
 }
-
-
 
 // login result
 class AuthorItem {
@@ -123,8 +131,7 @@ class AuthorItem {
     this.online,
   });
 
-  factory AuthorItem.fromJson(Map<String, dynamic> json) =>
-      AuthorItem(
+  factory AuthorItem.fromJson(Map<String, dynamic> json) => AuthorItem(
         token: json["token"],
         name: json["name"],
         description: json["description"],
@@ -137,19 +144,17 @@ class AuthorItem {
       );
 
   Map<String, dynamic> toJson() => {
-    "token": token,
-    "name": name,
-    "description": description,
-    "avatar": avatar,
-    "job": job,
-    "follow": follow,
-    "score": score,
-    "download": download,
-    "online": online,
-  };
-
+        "token": token,
+        "name": name,
+        "description": description,
+        "avatar": avatar,
+        "job": job,
+        "follow": follow,
+        "score": score,
+        "download": download,
+        "online": online,
+      };
 }
-
 
 // login result
 class CourseItem {
@@ -183,14 +188,15 @@ class CourseItem {
     this.id,
   });
 
-  factory CourseItem.fromJson(Map<String, dynamic> json) =>
-      CourseItem(
+  factory CourseItem.fromJson(Map<String, dynamic> json) => CourseItem(
         user_token: json["user_token"],
         name: json["name"],
         description: json["description"],
         thumbnail: json["thumbnail"],
         video: json["video"],
-        price: json["price"],
+        //! In backend the price is stored as decimal , but here it is stored as string
+        //! so we need to convert the decimal into string
+        price: json["price"].toString(),
         amount_total: json["amount_total"],
         lesson_num: json["lesson_num"],
         video_len: json["video_len"],
@@ -201,20 +207,18 @@ class CourseItem {
       );
 
   Map<String, dynamic> toJson() => {
-    "user_token": user_token,
-    "name": name,
-    "description": description,
-    "thumbnail": thumbnail,
-    "video": video,
-    "price": price,
-    "amount_total": amount_total,
-    "lesson_num": lesson_num,
-    "video_len": video_len,
-    "down_num": down_num,
-    "follow": follow,
-    "score": score,
-    "id": id,
-  };
-
+        "user_token": user_token,
+        "name": name,
+        "description": description,
+        "thumbnail": thumbnail,
+        "video": video,
+        "price": price,
+        "amount_total": amount_total,
+        "lesson_num": lesson_num,
+        "video_len": video_len,
+        "down_num": down_num,
+        "follow": follow,
+        "score": score,
+        "id": id,
+      };
 }
-
