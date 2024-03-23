b16ce663c393d5cce313bb60c5f0092a316b3f46
Vishal Ghosh <avatarvishal1551@gmail.com>
2024-03-23 19:32:40 +0530

    Course List Api Call Done

--- lib/common/utils/http_utils.dart
+++ lib/common/utils/http_utils.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:ecommerceapp/common/utils/constant.dart';
+import 'package:ecommerceapp/global.dart';
 
 class HttpUtil {
   // Singleton Class - only one instance of class will be created
@@ -27,13 +28,36 @@ class HttpUtil {
     String path, {
     dynamic mydata,
     Map<String, dynamic>? queryParameters,
+    // options allow us to pass headers which we use here to pass the
+    // access token from the local storage to the server to get authorization
+    // to use call the required api , that is wrapped around sanctum malwire protection
+
+    Options? options,
   }) async {
-    var response = await dio.post(
-      path,
-      data: mydata,
-      queryParameters: queryParameters,
-    );
-    // print("My response data is ${response.toString()}");
+    Options requestOptions = options ?? Options();
+    requestOptions.headers = requestOptions.headers ?? {};
+    Map<String, dynamic>? authorization = getAuthorizationHeader();
+    if (authorization != null) {
+      requestOptions.headers!.addAll(authorization);
+    }
+    var response = await dio.post(path,
+        data: mydata,
+        queryParameters: queryParameters,
+        options: requestOptions);
+    print("My response data is ${response.toString()}");
     return response.data;
+  }
+
+  Map<String, dynamic>? getAuthorizationHeader() {
+    var header = <String, dynamic>{};
+    var accessToken = Global.storageService.getUserToken();
+    if (accessToken.isNotEmpty) {
+      //* This authorization is similar to Authorization in postman
+      // and how we use bearer token to get access to use the courseList api
+      // The authorization speeling should be same as that of the postman
+      header['Authorization'] = 'Bearer $accessToken';
+    }
+    // Then we return the header which is in the form of <string , dynamic>
+    return header;
   }
 }
